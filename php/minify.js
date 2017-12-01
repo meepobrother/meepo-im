048c076205613fa076bfe79745ef4bf3d792f4d2
15138730370 <1037483576@qq.com>
2017-12-01 17:10:03 +0800

    save

--- php/minify.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const phpMinify = require('@aquafadas/gulp-php-minify');
-
-gulp.task('minify:php', () => gulp.src('path/to/lib/**/*.php', { read: false })
-    .pipe(phpMinify())
-    .pipe(gulp.dest('path/to/out'))
-);
